823cb85116fc6961f9787621c229bee0d12a105d
student <student@learncodinganywhere.com>
2020-12-09 21:03:39 -0800

    Add project files.

---
+++ ClassObjectsStep223/Math.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassObjectsStep223
+{
+    public class Math
+    {
+
+
+
+        public int mathstuff1( int A)
+        {
+            return A / 2;
+
+        }
+        public int mathstuff2(int B)
+        {
+            return B * 5;
+        }
+
+        public int mathstuff3(int C)
+        {
+            return C + 10;
+        }
+    }       
+}
---
+++ ClassObjectsStep223/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassObjectsStep223
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+          
+
+            Console.WriteLine(" What number you want to do the math operations.?\n ");
+            int mathOperations = Convert.ToInt32(Console.ReadLine());
+            int firstMath = math.mathstuff1(mathOperations);
+            int secondtMath = math.mathstuff2(mathOperations);
+            int ThirdMath = math.mathstuff3(mathOperations);
+            Console.WriteLine("First math: " + firstMath + "\nSecond math : " + secondtMath + "\nThird Math : " + ThirdMath);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
